90928dbac4f7ed7b9c9a240767d61efb1be79d52
shu-saginoya <saginoya@create-nippo.co.jp>
2023-04-07 16:54:26 +0900

    Update - Contents

--- src/components/sectionPR.vue
+++ src/components/sectionPR.vue
@@ -1,11 +1,122 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const PRs = [
+  {
+    title: '経歴の遍歴について',
+    body: '小学生のころから台本の制作などをはじめ、高校生の頃は市民劇団に所属し、専門学校では文芸創作科という学科に通っていました。はじめに入社した会社はDTP主体の出版系の会社で、2社目の会社では映像制作のためシナリオの制作などに注力していました。その後、短期間ですが同じ会社で営業職を経験し、その際勉強したWEBの知識を活かして、現在（3社目）の会社でWEB制作の仕事に就いています。現在の会社は私が入社した当時はいわゆるWEBサイト制作を業務としていて技術はHTMLとCSSだけという状況でしたが、前職の知識を活かしてレスポンシブデザインやWordpressの導入などを行い、最近ではJavaScriptのフレームワークを導入したり、モダンな開発環境を導入できるように尽力しております。'
+  },
+  {
+    title: 'エンジニアとして',
+    body: '高校の学科が情報技術科という学部でITの基礎やC言語などを学びました。ただ、この当時はあまりそれを仕事にするという意識はなく、卒業後はプログラミングから離れてしまいました。それからはじめに入社した会社でアプリケーション開発（PHP+MySQL）を突然任せられるなどの突発的なことはありましたが、コードを本格的に書くようになったのは現在のWEB制作の職についてからです。ただそれからも、独学だったこともあり次にどのような技術を導入していくかということに悩まされる期間が長くつづきます。そんななかで出会ったフレームワークに大きな衝撃を受けました。いろいろと検討した結果、まずはフロントエンドということでVue.jsを導入しました。それをきっかけに必要な技術や開発環境の課題が明確になりコロナ禍のなか夢中で勉強しました。そのため、現在の一番の得意分野はフロントエンドで開発環境の構築やコンポーネントの設計などは自信がもてるようになりました。'
+  },
+  {
+    title: 'サンプル',
+    body: '外部に公開できないソースコードが多いためサンプルが少ないのですが参考にしていただければと思います。',
+    list: [
+      {
+        name: '書籍の購読申込フォーム (Vue3・Typescript・SCSSなど)',
+        src: 'https://github.com/shu-saginoya/example-form'
+      },
+      {
+        name: 'リマインダー(Nuxt3・まだ開発途中でお見苦しく申し訳ありません)',
+        src: 'https://github.com/shu-saginoya/shared-reminder'
+      },
+      {
+        name: '大学図書館（Nuxt2・microCMS）',
+        src: 'https://www.library.ferris.ac.jp/'
+      },
+      {
+        name: '真空ポンプの会社(Wordpress)',
+        src: 'http://www.skott.co.jp/'
+      }
+    ]
+  },
+  {
+    title: '参考書籍',
+    body: '',
+    list: [
+      {
+        name: '『HTML解体新書　仕様から紐解く本格入門』',
+        src: '著・太田良典、中村直樹（ボーンデジタル）'
+      },
+      {
+        name: '『CSS設計完全ガイド　詳細解説＋実践的モジュール集』',
+        src: '著・半田惇志（技術評論社）'
+      },
+      {
+        name: '『ステップアップJavaScript　フロントエンド開発の初級から中級へ進むために』',
+        src: '著・サークルアラウンド株式会社、佐藤正志、小笠原寛（翔泳社）'
+      },
+      {
+        name: '『TypeScriptハンズオン』',
+        src: '著・掌田津耶乃（秀和システム）'
+      },
+      {
+        name: '『Vue.jsプロフェッショナルWebプログラミング』',
+        src: '著・山田典明、長澤賢、関口勇樹（エムディーエヌコーポレーション）'
+      },
+      {
+        name: '『Vue.js入門　基礎から実践アプリケーション開発まで』',
+        src: '著・川口和也、喜多啓介、野田陽平、手島拓也、片山信也（技術評論社）'
+      },
+      {
+        name: '『Vue 3 フロントエンド開発の教科書',
+        src: '著・WINGS プロジェクト、齊藤新三（技術評論社）'
+      },
+      {
+        name: '『フロントエンド開発入門　プロフェッショナルな開発ツールと設計・実装』',
+        src: '著・安達稜、武田諭（秀和システム）'
+      },
+      {
+        name: '『実践力を身につけるPythonの教科書』',
+        src: '著・クジラ飛行机（マイナビ出版）'
+      },
+      {
+        name: '『Python　Django3　超入門』',
+        src: '著・掌田津耶乃（秀和システム）'
+      },
+      {
+        name: '『プロフェッショナルWebプログラミング　Laravel』',
+        src: '著・久保田 賢二朗、荒井 和平、大橋 佑太（エムディエヌコーポレーション）'
+      },
+      {
+        name: '『WEB＋DB PRESS Vol.120』（技術評論社）',
+        src: '主な特集「自作OS×自作ブラウザで学ぶWebページが表示されるまで」「最新Vue.js3　基本から新機能まで完全網羅」'
+      },
+      {
+        name: '『Software Design 2022年2月号』（技術評論社）',
+        src: '主な特集「JavaScriptの関数を極める」「GitHub Actionsで簡単・快適CI／CD」'
+      }
+    ]
+  }
+]
+</script>
 
 <template>
   <section class="bl_container">
     <h2>自己PR</h2>
-    <section class="bl_card">
-      <h3>タイトル</h3>
-      <p>内容</p>
+    <section v-for="(pr, index) in PRs" :key="index" class="bl_card">
+      <h3>{{ pr.title }}</h3>
+      <p>{{ pr.body }}</p>
+      <ul v-if="pr.list" class="bl_list">
+        <li v-for="(item, listIndex) in pr.list" :key="listIndex" class="bl_list_item">
+          <div>{{ item.name }}</div>
+          <div>{{ item.src }}</div>
+        </li>
+      </ul>
     </section>
   </section>
 </template>
+
+<style scoped lang="scss">
+.bl_list {
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  list-style: none;
+  padding-left: 0;
+  font-size: 0.9rem;
+  &_item {
+    border-top: gray solid 1px;
+  }
+}
+</style>
--- src/components/sectionSkill.vue
+++ src/components/sectionSkill.vue
@@ -12,27 +12,32 @@ const skillList = [
       {
         name: 'CSS',
         started: '2014-09',
-        note: '命名規則やモジュールの責任範囲を考慮しながら設計しています。SassはSCSS記法'
+        note: '命名規則やモジュールの責任範囲を考慮しながら設計しています。Sassも利用しています。'
       },
       {
         name: 'JavaScript',
         started: '2014-09',
-        note: ''
+        note: 'シンプルでわかりやすいコードを心掛けています。'
       },
       {
         name: 'TypeScript',
         started: '2022-04',
-        note: ''
+        note: 'Vue.jsのバージョン3導入と同時に業務に導入しました。作業が楽になり重宝しています。'
       },
       {
         name: 'Vue.js',
         started: '2021-04',
-        note: ''
+        note: '段階的な導入が容易という点に惹かれて導入しました。最近ではバージョン2.x系から3.x系への移行にともない、composition APIとTypeScriptの導入で業務効率がアップしたように感じます。日本ユーザーグループなどのイベントにも参加することがあります。'
       },
       {
         name: 'Nuxt',
         started: '2021-04',
-        note: ''
+        note: '大学図書館の案件でmicroCMSとNetlifyと組み合わせてJamstack構成で運営しています。3.0系についても試験的に利用しています。'
+      },
+      {
+        name: 'その他・開発環境など',
+        started: '-',
+        note: 'VSCode	Git	GitHub SourceTree	Node.js npm	webpack	babel	Vite	ESLint Prettier	Volta	Volar Vitest'
       }
     ]
   },
@@ -43,27 +48,48 @@ const skillList = [
       {
         name: 'PHP',
         started: '2016-09',
-        note: 'なるべく最新のリファレンスを気にかけ、アクセシビリティやSEOにも配慮しながらコーディングしています。'
+        note: '簡易的なCMSや検索システム・メール送信システムの制作などで利用してきました。'
       },
       {
         name: 'WordPress',
         started: '2016-09',
-        note: '命名規則やモジュールの責任範囲を考慮しながら設計しています。SassはSCSS記法'
+        note: '数年前までは業務の中心でした。'
       },
       {
         name: 'Laravel',
-        started: '2014-09',
-        note: ''
+        started: '実務なし',
+        note: '技術選定の一環で簡単なアプリの制作経験あり。'
       },
       {
         name: 'Python',
         started: '2022-04',
-        note: ''
+        note: '基本的なプログラミングが可能。ローカル環境での事務作業の自動化など。'
       },
       {
         name: 'Django',
-        started: 'なし',
-        note: ''
+        started: '実務なし',
+        note: '技術選定の一環で簡単なアプリの制作経験あり。'
+      },
+      {
+        name: 'その他・開発環境など',
+        started: '-',
+        note: 'WSL Docker Ubuntu MySQL'
+      }
+    ]
+  },
+  {
+    name: 'そのほか',
+    id: 'others',
+    body: [
+      {
+        name: 'DTP',
+        started: '-',
+        note: '書籍の版組やイラストの制作など。Adobe InDesign・Illustrator・Photoshop'
+      },
+      {
+        name: '映像制作',
+        started: '-',
+        note: '企画・シナリオ・絵コンテなどの制作から、業務用ビデオカメラでの撮影、Adobe Premiere ProやAfter Effectsでの編集、撮影時やナレーション収録時のディレクションなどが可能。'
       }
     ]
   }
