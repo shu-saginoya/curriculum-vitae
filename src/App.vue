4c75eb3db25f2c1690e0e3521271eb223a64d19b
Shu Saginoya <shu.saginoya@gmail.com>
2023-10-18 18:03:15 +0900

    Update - App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,25 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 
-const updateDate = '2023-05-27'
+const updateDate = '2023年10月18日'
+const name = '鷺野谷周'
 </script>
 
 <template>
-  <header class="bl_header">
-    <h1>職務経歴書</h1>
-    <p>
-      更新日：<time :datetime="updateDate">{{ updateDate }}</time>
-    </p>
-  </header>
-
-  <RouterView />
+  <div class="max-w-4xl mx-auto py-8">
+    <header>
+      <h1 class="text-xl font-bold text-center">職務経歴書</h1>
+      <ul class="text-right">
+        <li>
+          <time>{{ updateDate }}</time
+          >現在
+        </li>
+        <li>氏名：{{ name }}</li>
+      </ul>
+    </header>
+    <RouterView />
+    <footer>
+      <p class="text-right">以上</p>
+    </footer>
+  </div>
 </template>
-
-<style scoped lang="scss">
-.bl_header {
-  display: flex;
-  justify-content: space-between;
-  align-items: flex-end;
-}
-</style>
