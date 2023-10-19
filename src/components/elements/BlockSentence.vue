b0566cee8a2c7bd5c4fd2f00a13cbc1299534438
saginoya <saginoya@create-nippo.co.jp>
2023-10-19 16:03:47 +0900

    Update - SectionPR

--- src/components/elements/BlockSentence.vue
+++ src/components/elements/BlockSentence.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 defineProps<{
   items: string[]
+  title?: string
 }>()
 </script>
 
 <template>
   <div class="px-4">
+    <h3 v-if="title" class="font-bold">◆{{ title }}</h3>
     <p v-for="(item, index) in items" :key="index">
       {{ item }}
     </p>
