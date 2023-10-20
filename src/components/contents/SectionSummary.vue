36c56a96ed868d935a99c8610f41010693561a05
saginoya <saginoya@create-nippo.co.jp>
2023-10-20 14:22:17 +0900

    Update - SectionSummary

--- src/components/contents/SectionSummary.vue
+++ src/components/contents/SectionSummary.vue
@@ -3,7 +3,10 @@ import MainSection from '@/components/elements/MainSection.vue'
 import BlockSentence from '@/components/elements/BlockSentence.vue'
 
 const title = '職務要約'
-const sentence = ['aaaa', 'bbbbb']
+const sentence = [
+  '2014年、株式会社クリエイト日報に入社しました。',
+  '入社後5年間、主にHTML/CSS/JavaScript/PHPを用いたwebサイト/webアプリの基本設計/詳細設計/コーディング/運用保守に従事し、入社後5年以降はVue.js/TypeScriptやモダンな開発環境の導入など積極的な技術導入をリードしてきました。'
+]
 </script>
 
 <template>
