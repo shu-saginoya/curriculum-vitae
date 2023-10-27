65c650a66753da92c255a7b62aa105d0e1406419
saginoya <saginoya@create-nippo.co.jp>
2023-10-27 15:57:39 +0900

    Update - SectionSkill

--- src/components/contents/SectionSkill.vue
+++ src/components/contents/SectionSkill.vue
@@ -9,14 +9,14 @@ const title = 'PCスキル／テクニカルスキル'
 const items = [
   {
     cat: '担当業務',
-    class: ['実装', 'テスト', '運用保守'],
-    years: [yearsExp('2014-09'), 0, yearsExp('2014-09')],
+    class: ['実装', '運用保守'],
+    years: [yearsExp('2014-09'), yearsExp('2014-09')],
     note: ['メンバーとしてクライアントとのやり取り経験あり']
   },
   {
     cat: 'OS',
     class: ['Windows'],
-    years: [yearsExp('1994-06')],
+    years: [yearsExp('2014-09')],
     note: ['通常使用に問題なしで、指導も可能']
   },
   {
@@ -41,25 +41,20 @@ const items = [
   },
   {
     cat: 'フレームワーク',
-    class: ['Vue.js', 'Nuxt', 'Laravel', 'Django'],
-    years: [yearsExp('2021-04'), yearsExp('2021-04'), 0, 0],
-    note: [
-      '通常使用に問題なしで、指導も可能',
-      '通常使用に問題なしで、指導も可能',
-      '書籍に従いアプリケーション制作経験',
-      '書籍に従いアプリケーション制作経験'
-    ]
+    class: ['Vue.js', 'Nuxt'],
+    years: [yearsExp('2021-04'), yearsExp('2021-04')],
+    note: ['通常使用に問題なしで、指導も可能', '通常使用に問題なしで、指導も可能']
   },
   {
     cat: 'DB',
-    class: ['MySQL', 'SQLite'],
-    years: [yearsExp('2016-09'), 0],
-    note: ['調べながらであれば作業可能', '調べながらであれば作業可能']
+    class: ['MySQL'],
+    years: [yearsExp('2016-09')],
+    note: ['調べながらであれば作業可能']
   },
   {
     cat: 'その他',
     class: ['Git', 'GitHub', 'WordPress', 'Docker', 'Node.js'],
-    years: [yearsExp('2021-04'), yearsExp('2021-04'), yearsExp('2016-09'), 0, yearsExp('2021-04')],
+    years: [yearsExp('2021-04'), yearsExp('2021-04'), yearsExp('2016-09'), 1, yearsExp('2021-04')],
     note: [
       '通常使用に問題なし',
       '通常使用に問題なし',
